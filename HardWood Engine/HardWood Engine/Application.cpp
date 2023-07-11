cf44bfcfafb245b9c9b65c950df6abd195f9634a
Kody Wood <kodymrwood14@gmail.com>
2023-07-11 11:04:02 -0400

    Edited the Application, Added proprietary math library.

--- HardWood Engine/HardWood Engine/Application.cpp
+++ HardWood Engine/HardWood Engine/Application.cpp
@@ -2,14 +2,57 @@
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 
+//OpenGL Dependencies
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <stdio.h>
+
+#include "wMath.h"
+
+static const struct
+{
+    float x, y;
+    float r, g, b;
+} vertices[3] =
+{
+    { -0.6f, -0.4f, 1.f, 0.f, 0.f },
+    {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {   0.f,  0.6f, 0.f, 0.f, 1.f }
+};
+
+//--- Vertex Shader ---//
+static const char* vertex_shader_text =
+"#version 110\n"
+"uniform mat4 MVP;\n"
+"attribute vec3 vCol;\n"
+"attribute vec2 vPos;\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    color = vCol;\n"
+"}\n";
+
+//--- Fragment Shader ---//
+static const char* fragment_shader_text =
+"#version 110\n"
+"varying vec3 color;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
+"}\n";
 
 
 void Error_Callback(int error, const char* description)
 {
     fprintf(stderr, "Error: %s\n", description);
+}
+
+static void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 int main(void)
---
+++ HardWood Engine/HardWood Engine/wMath.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/*
+* Description - This header is to define all math functions that are used in graphics and 3D math. Simply a math header
+* 
+* Author - Kody Wood
+* 
+*/
